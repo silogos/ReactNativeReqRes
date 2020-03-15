a3f10d2aaa902df4243cd141f85bc3a1e1bf8e19
Amin Yusuf <si.logoz@gmail.com>
2020-03-15 23:59:49 +0700

    add Styles

---
+++ src/styles/Colors.js
@@ -0,0 +1,45 @@
+const Colors = {
+  primary: {
+    black: 'rgba(0,0,0,1)',
+    white: 'rgba(255,255,255,1)',
+    gray: 'rgba(10,60,120,1)',
+    blue: 'rgba(10,60,120,1)',
+    black: '#000',
+  },
+
+  dark: {
+    white: 'rgba(255,255,255,0.25)',
+    black: 'rgba(0,0,0,.25)',
+  },
+
+  light: {
+    blue: 'rgba(10,160,225, 0.12)',
+  },
+
+  blue: '#0aa0e1',
+  green: '#69b946',
+  grey: '#b6bac1',
+  grey_2: '#dbe1e7',
+  grey_3: '#f3f7f8',
+  grey_4: '#ededed',
+  grey_5: '#999999',
+  grey_6: '#f5f8f9',
+  grey_7: '#c9d2d4',
+  grey_8: '#E1E1E1',
+  grey_9: '#a1a1a1',
+  grey_10: '#e0e5ea',
+  grey_11: '#A6A6A6',
+  grey_12: '#B9B9B9',
+  grey_13: '#f9f9f9',
+  main_color: '#0a3c78',
+  main_color_2: '#0a3264',
+  main_color_3: '#072d5a',
+  orange: '#f29e30',
+  red: '#e10a0a',
+  border: '#e2eaec',
+  purple: '#9A55CE',
+
+  borderButton: '#e1e1e1',
+};
+
+export default Colors;
